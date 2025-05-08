9d9f8fb03894c50aeca97c9e2f948f5d78e97f02
Herbie Smith <herbert.smith@netwrix.com>
2025-05-08 09:14:05 +0100

    Update AssemblyInfo for PingCastleAutoUpdater to match correct version and copyright.

--- PingCastleAutoUpdater/Properties/AssemblyInfo.cs
+++ PingCastleAutoUpdater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Netwrix Corporation")]
 [assembly: AssemblyProduct("PingCastle")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © Netwrix Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.3.0.11")]
+[assembly: AssemblyFileVersion("3.3.0.11")]

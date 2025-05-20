548de82eb0d9631e7a244deffaaf63cfcd4fb460
HerbieSmith-Netwrix <Herbert.Smith@netwrix.com>
2025-05-20 20:34:45 +0100

    PingCastle 3.3.0.12 (#287)
    - Fixed EOL Date
    - Changelog update to point to Netwrix community
    
    ---------
    
    Signed-off-by: HerbieSmith-Netwrix <Herbert.Smith@netwrix.com>

--- PingCastleAutoUpdater/Properties/AssemblyInfo.cs
+++ PingCastleAutoUpdater/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.11")]
-[assembly: AssemblyFileVersion("3.3.0.11")]
+[assembly: AssemblyVersion("3.3.0.12")]
+[assembly: AssemblyFileVersion("3.3.0.12")]

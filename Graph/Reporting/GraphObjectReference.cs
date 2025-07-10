64f4968886407c0b5afd721d4a238c5295576745
Herbie Smith <herbert.smith@netwrix.com>
2025-07-10 15:39:18 +0100

    Manually updated from master

--- Graph/Reporting/GraphObjectReference.cs
+++ Graph/Reporting/GraphObjectReference.cs
@@ -1,5 +1,6 @@
 ﻿using PingCastle.ADWS;
 using PingCastle.Data;
+using System;
 using System.Collections.Generic;
 
 namespace PingCastle.Graph.Reporting
@@ -12,7 +13,6 @@ namespace PingCastle.Graph.Reporting
             Description = description;
             Risk = risk;
         }
-
         public string Name { get; set; }
         public string Description { get; set; }
         public CompromiseGraphDataObjectRisk Risk { get; set; }
@@ -20,6 +20,7 @@ namespace PingCastle.Graph.Reporting
 
     public class GraphObjectReference
     {
+
         public const string DomainAdministrators = "Domain Administrators";
         public const string Administrators = "Administrators";
         public const string AccountOperators = "Account Operators";
@@ -55,55 +56,52 @@ namespace PingCastle.Graph.Reporting
 
         public GraphObjectReference(ADDomainInfo data)
         {
-            Objects = new Dictionary<CompromiseGraphDataTypology, List<GraphSingleObject>>
+            Objects = new Dictionary<CompromiseGraphDataTypology, List<GraphSingleObject>>()
             {
-                {
-                    CompromiseGraphDataTypology.PrivilegedAccount, new List<GraphSingleObject>
-                    {
-                        new GraphSingleObject("S-1-5-32-544", Administrators, CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject("S-1-5-32-548", AccountOperators, CompromiseGraphDataObjectRisk.High),
-                        new GraphSingleObject("S-1-5-32-549", ServerOperators, CompromiseGraphDataObjectRisk.High),
-                        new GraphSingleObject("S-1-5-32-550", PrintOperators, CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("S-1-5-32-551", BackupOperators, CompromiseGraphDataObjectRisk.High),
-                        new GraphSingleObject("S-1-5-32-569", CertificateOperators, CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("S-1-5-32-552", Replicator, CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject(data.DomainSid.Value + "-500", Administrator, CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject(data.DomainSid.Value + "-512", DomainAdministrators, CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject(data.DomainSid.Value + "-517", CertificatePublishers),
-                        new GraphSingleObject(data.DomainSid.Value + "-518", SchemaAdministrators, CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject(data.DomainSid.Value + "-519", EnterpriseAdministrators, CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject(data.DomainSid.Value + "-526", KeyAdministrators, CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject(data.DomainSid.Value + "-527", EnterpriseKeyAdministrators, CompromiseGraphDataObjectRisk.Medium),
-                    }
-                },
-                {
-                    CompromiseGraphDataTypology.Infrastructure, new List<GraphSingleObject>
-                    {
-                        new GraphSingleObject(data.DomainSid.Value, "Domain Root", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject(data.DomainSid.Value + "-498", "Enterprise Read Only Domain Controllers"),
-                        new GraphSingleObject(data.DomainSid.Value + "-502", "Krbtgt account", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject(data.DomainSid.Value + "-516", "Domain Controllers", CompromiseGraphDataObjectRisk.Critical),
-                        new GraphSingleObject(data.DomainSid.Value + "-520", "Group Policy Creator Owners", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject(data.DomainSid.Value + "-521", "Read Only Domain Controllers", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("CN=Builtin," + data.DefaultNamingContext, "Builtin OU", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("CN=Users," + data.DefaultNamingContext, "Users container", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("CN=Computers," + data.DefaultNamingContext, "Computers container", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("CN=NTAuthCertificates,CN=Public Key Services,CN=Services," + data.ConfigurationNamingContext, "Certificate store", CompromiseGraphDataObjectRisk.Medium),
-                        new GraphSingleObject("CN=AdminSDHolder,CN=System," + data.DefaultNamingContext, "AdminSDHolder object", CompromiseGraphDataObjectRisk.Critical),
-                    }
-                },
-                {
-                    CompromiseGraphDataTypology.UserDefined, new List<GraphSingleObject>()
-                }
+                {CompromiseGraphDataTypology.PrivilegedAccount, new List<GraphSingleObject>(){
+                    new GraphSingleObject("S-1-5-32-544", Administrators, CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject("S-1-5-32-548", AccountOperators, CompromiseGraphDataObjectRisk.High),
+                    new GraphSingleObject("S-1-5-32-549", ServerOperators, CompromiseGraphDataObjectRisk.High),
+                    new GraphSingleObject("S-1-5-32-550", PrintOperators, CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("S-1-5-32-551", BackupOperators, CompromiseGraphDataObjectRisk.High),
+                    new GraphSingleObject("S-1-5-32-569", CertificateOperators, CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("S-1-5-32-552", Replicator, CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject(data.DomainSid.Value + "-500", Administrator, CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject(data.DomainSid.Value + "-512", DomainAdministrators, CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject(data.DomainSid.Value + "-517", CertificatePublishers),
+                    new GraphSingleObject(data.DomainSid.Value + "-518", SchemaAdministrators, CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject(data.DomainSid.Value + "-519", EnterpriseAdministrators, CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject(data.DomainSid.Value + "-526", KeyAdministrators, CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject(data.DomainSid.Value + "-527", EnterpriseKeyAdministrators, CompromiseGraphDataObjectRisk.Medium),
+                }},
+                {CompromiseGraphDataTypology.Infrastructure, new List<GraphSingleObject>(){
+                    new GraphSingleObject(data.DomainSid.Value,"Domain Root", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject(data.DomainSid.Value + "-498","Enterprise Read Only Domain Controllers"),
+                    new GraphSingleObject(data.DomainSid.Value + "-502","Krbtgt account", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject(data.DomainSid.Value + "-516","Domain Controllers", CompromiseGraphDataObjectRisk.Critical),
+                    new GraphSingleObject(data.DomainSid.Value + "-520","Group Policy Creator Owners", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject(data.DomainSid.Value + "-521","Read Only Domain Controllers", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("CN=Builtin," + data.DefaultNamingContext,"Builtin OU", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("CN=Users," + data.DefaultNamingContext,"Users container", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("CN=Computers," + data.DefaultNamingContext,"Computers container", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("CN=NTAuthCertificates,CN=Public Key Services,CN=Services," + data.ConfigurationNamingContext,"Certificate store", CompromiseGraphDataObjectRisk.Medium),
+                    new GraphSingleObject("CN=AdminSDHolder,CN=System," + data.DefaultNamingContext,"AdminSDHolder object", CompromiseGraphDataObjectRisk.Critical),
+                }},
+                {CompromiseGraphDataTypology.UserDefined, new List<GraphSingleObject>(){
+                }},
             };
 
             foreach (var typology in Objects.Keys)
             {
-                Objects[typology].Sort((a, b)
-                    => string.CompareOrdinal(a.Description, b.Description));
+                Objects[typology].Sort((GraphSingleObject a, GraphSingleObject b)
+                    =>
+                {
+                    return String.Compare(a.Description, b.Description);
+                });
             }
-
-            TechnicalObjects = new List<string> { data.DomainSid.Value + "-525" };
+            TechnicalObjects = new List<string>();
+            TechnicalObjects.Add(data.DomainSid.Value + "-525");
         }
     }
+
 }
